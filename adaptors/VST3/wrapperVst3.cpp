9c46042a036587e3b5da6d7e8e45b53753824e73
Jeff McClintock <jef@synthedit.com>
2024-07-01 16:11:02 +1200

    mac VST3 passing validator

--- adaptors/VST3/wrapperVst3.cpp
+++ adaptors/VST3/wrapperVst3.cpp
@@ -1,5 +1,27 @@
 #include "MyVstPluginFactory.h"
 
+#ifndef _WIN32
+#include <CoreFoundation/CoreFoundation.h>
+
+bool bundleEntry_internal (CFBundleRef ref);
+bool bundleExit_internal (void);
+
+
+extern "C" {
+/** bundleEntry and bundleExit must be provided by the plug-in! */
+SMTG_EXPORT_SYMBOL bool bundleEntry (CFBundleRef r)
+{
+    return bundleEntry_internal(r);
+}
+
+SMTG_EXPORT_SYMBOL bool bundleExit (void)
+{
+    return bundleExit_internal();
+}
+
+}
+#endif
+
 // need to export the factory symbol from the plugin DLL, the static library can't export symbols on mac.
 SMTG_EXPORT_SYMBOL Steinberg::IPluginFactory* PLUGIN_API GetPluginFactory()
 {
@@ -8,3 +30,5 @@ SMTG_EXPORT_SYMBOL Steinberg::IPluginFactory* PLUGIN_API GetPluginFactory()
 
 bool InitModule() { return true; }
 bool DeinitModule() { return true; }
+
+
